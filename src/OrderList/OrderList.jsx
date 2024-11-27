6c282e2108e1223aa6e5cfbeb2a8475ab96fb44d
sarunasbit <sarunas.bit@gmail.com>
2024-11-27 21:43:01 +0200

    test

--- src/OrderList/OrderList.jsx
+++ src/OrderList/OrderList.jsx
@@ -2,12 +2,24 @@ import OrderItem from "../OrderItem/OrderItem";
 import OrderForm from "../orderForm/orderForm";
 import { useState } from "react";
 function OrderList() {
-
     const[isFormOpen, setIsFormOpen] = useState(false);
-    const[tableItem, setTableItem] = useState([]);
+    const[tableItem, setTableItem] = useState(() =>{
+        return JSON.parse(localStorage.getItem("storedItems")) || [];
+    });
     const openForm = () => {setIsFormOpen(!isFormOpen)};
 
+    const storedItems = JSON.parse(localStorage.getItem("storedItems")) || [];
+
+    const updateLocalStorage = (updatedItems) => {
+        localStorage.setItem("storedItems", JSON.stringify(updatedItems));
+    };
+    const getLocalStorage = () => {
+        return JSON.parse(localStorage.getItem('storedItems'))
+    }
+    
     const addForm = (data) => {
+        storedItems.push(data)
+        updateLocalStorage(storedItems)
         setTableItem((prevData)=> {
             return[...prevData, data]
         })
@@ -15,8 +27,21 @@ function OrderList() {
     }
 
     const toggleStatus = (data) => {
-        console.log(data.active);
-    
+        const test = getLocalStorage()
+        test.map(items => 
+        {
+            if(items.id == data.id){
+                if(!items.active){
+                    items.active = "on"
+                } else{
+                    items.active = false
+                }
+            }
+        }
+        )
+        updateLocalStorage(test)
+
+
         setTableItem((prevData) =>
             prevData.map((item) =>
                 item.id === data.id
@@ -26,13 +51,46 @@ function OrderList() {
         );
     };
 
+    const getAllItems = () => {
+        const storedItems = getLocalStorage()
+        if(storedItems == null) {
+            return (
+                <tr>
+                    <td>No orders</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+            </tr>
+            )
+        }
+        setTableItem(storedItems)
+    }
+
+    const getActiveItems = () => {
+        const storedItems = getLocalStorage()
+        const activeItems = storedItems.filter(item=> item.active === "on");
+        setTableItem(activeItems)
+        console.log(storedItems)
+    }
+
+    const getInactiveItems = () => {
+        const storedItems = getLocalStorage()
+        const inactiveItems = storedItems.filter(item => item.active === false)
+        setTableItem(inactiveItems)
+    }
+
+    // const dumpLocalStorage = () => {
+    //     localStorage.removeItem("storedItems")
+    // }
+
     return (
         <>
         <div className="row mb-3 mt-3 d-flex justify-content-center">
         <div className="btn-group col-3" role="group" aria-label="Basic example">
-            <button type="button" name="all" className="btn btn-secondary">All</button>
-            <button type="button" name="active" className="btn btn-secondary">Active</button>
-            <button type="button" name="inactive" className="btn btn-secondary">Inactive</button>
+            <button type="button" name="all" onClick={getAllItems} className="btn btn-secondary">All</button>
+            <button type="button" name="active" onClick={getActiveItems} className="btn btn-secondary">Active</button>
+            <button type="button" name="inactive" onClick={getInactiveItems} className="btn btn-secondary">Inactive</button>
+            {/* <button type="button" name="dump" onClick={dumpLocalStorage} className="btn btn-secondary">Dump Storage</button> */}
         </div>
         </div>
         <table className="table">
